8d3bde8d8a9b15fb861c0c8dde0d5eb37a100652
HolgerBartel <54469387+HolgerBartel@users.noreply.github.com>
2020-06-07 00:25:06 +0200

    specify alpha in models

--- models.py
+++ models.py
@@ -24,6 +24,7 @@ def example():
         "ymvars": [Y3],                         # manifest endogenous variables
         "final_var": Y3,                        # final variable of interest, for mediation analysis
         "show_nr_indiv": 3,                     # show first individual effects
+        "alpha": None,                          # regularization parameter, is estimated if None
         "dir_path": "output/",                  # output directory path
         }
 
@@ -76,6 +77,7 @@ def example2():
         "ymvars": [Y1],
         "final_var": Y1,
         "show_nr_indiv": 3,
+        "alpha": None,
         "dir_path": "output/",
         }
 
@@ -121,6 +123,7 @@ def example3():
         "ymvars": [Y3],
         "final_var": Y3,
         "show_nr_indiv": 3,
+        "alpha": None,
         "dir_path": "output/",
         }
 
@@ -142,71 +145,20 @@ def example3():
     return model_dat
 
 def education():
-    """Education
-    
-    Gary Koop and Justin L. Tobias, "Learning about Heterogeneity in Returns
-    to Schooling", Journal of Applied Econometrics, Vol. 19, No. 7, 2004,
-    pp. 827-849.
-    https://www.economics.uci.edu/files/docs/workingpapers/2001-02/Tobias-07.pdf
-
-    This panel data set consists of NT=17,919 observations from  N=2,178
-    individuals. It contains the wage earnings history for young workers in
-    the U.S. from 1979 until 1993. The data are taken from the National
-    Longitudinal Survey of Youth (NLSY).
-    http://people.stern.nyu.edu/wgreene/Econometrics/PanelDataSets.htm
-    
-    The NLSY is a rich panel study of 12,686 individuals in the U.S. ranging in
-    age from 14-22 as of the first interview date in 1979. It contains detailed
-    information on the earnings and wages, educational attainment, family
-    characteristics, and test scores of the sampled individuals.
-    
-    Koop and Tobias (2004) use a version of the NLSY which allows to obtain an
-    earnings history until 1993. To abstract from selection issues in
-    employment, and to remain consistent with the majority of the literature,
-    they focus on the outcomes of white males in the NLSY. They restrict
-    attention to those individuals who are active in the labor force for a good
-    portion of each year, being at least 16 years of age in the given year, who
-    reported working at least 30 weeks a year and at least 800 hours per year.
-    They also deleted observations when the reported hourly wage is less than
-    $1 or greater than $100 dollars per hour, when education decreases across
-    time for an individual, or when the reported change in years of schooling
-    over time is not consistent with the change in time from consecutive
-    interviews. As such, they are careful to delete individuals whose education
-    is clearly mis-measured.   
-    
-    The dataset contains following variables in this order:
-    
-    Time Varying
-    0) PERSONID = Person id (ranging from 1 to 2,178) # not used by us
-    1) EDUC = Education (years of schooling)
-    2) LOGWAGE = Log of hourly wage, at most recent job, in real 1993 dollars
-    3) POTEXPER = Potential experience (= age - EDUC - 5)
-    4) TIMETRND = Time trend (starting at 1 in 1979 and incrementing by year)
-    
-    Time Invariant
-    5) ABILITY = Ability (cognitive ability measured by test score*)
-    6) MOTHERED = Mother's education (highest grade completed, in years)
-    7) FATHERED = Father's education (highest grade completed, in years)
-    8) BRKNHOME = Dummy variable for residence in a broken home at age 14
-    9) SIBLINGS = Number of siblings
-    
-    * constructed from the 10 component tests of the Armed Services Vocational
-    Aptitude Battery (ASVAB) administered to the NLSY participants in 1980.
-    Since individuals varied in age, each of the 10 tests is first residualized
-    on age, and the test score is defined as the first principal
-    component of the standardized residuals, standardized.
-    """
+    """Education"""
 
     (FATHERED, MOTHERED, SIBLINGS, BRKNHOME, ABILITY, AGE,
      EDUC, POTEXPER, LOGWAGE) = symbols(
          ["FATHERED", "MOTHERED", "SIBLINGS", "BRKNHOME", "ABILITY", "AGE",
           "EDUC", "POTEXPER", "LOGWAGE"])
     
+    # note that in Sympy some operators are special, e.g. Max() instead of max()
     from sympy import Max
 
     def define_equations(FATHERED, MOTHERED, SIBLINGS, BRKNHOME, ABILITY, AGE):
         
-        eq_EDUC = 12 + 0.1 * (FATHERED - 12) + 0.1 * (MOTHERED - 12) - 0.1 * SIBLINGS - 0.1 * BRKNHOME
+        eq_EDUC = (12 + 0.1 * (FATHERED - 12) + 0.1 * (MOTHERED - 12)
+                   - 0.1 * SIBLINGS - 0.1 * BRKNHOME)
         eq_POTEXPER = Max(AGE - EDUC - 5, 0)
         eq_LOGWAGE = 1.5 + 0.1 * (EDUC - 12) + 0.1 * POTEXPER + 0.1 * ABILITY
 
@@ -219,17 +171,18 @@ def education():
         "ymvars": [EDUC, POTEXPER, LOGWAGE],
         "final_var": LOGWAGE,
         "show_nr_indiv": 3,
+        "alpha": 16476.332322,
         "dir_path": "output/",
         }
 
     # load data
     from numpy import array, concatenate, loadtxt
     xymdat = loadtxt("data/education.csv", delimiter=",").reshape(-1, 10)
-    xymdat = xymdat.T # observation in columns
-    obs = 200
-    xymdat = xymdat[:, 0:obs] # just some of the 17,919 observations # yyyy
+    xymdat = xymdat.T # observations in columns
+    #xymdat = xymdat[:, 0:200] # just some of the 17,919 observations
     xdat = xymdat[[7, 6, 9, 8, 5]] # w/o PERSONID, TIMETRND
-    age = array(xymdat[3, :] + xymdat[1, :] + 5).reshape(1, -1) # age = POTEXPER + EDUC + 5
+    # age = POTEXPER + EDUC + 5
+    age = array(xymdat[3, :] + xymdat[1, :] + 5).reshape(1, -1)
     ymdat = xymdat[[1, 3, 2]]
     xdat = concatenate((xdat, age))
     
@@ -237,4 +190,3 @@ def education():
     model_dat["ymdat"] = ymdat
 
     return model_dat
-
