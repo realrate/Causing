6e545f2bc0b065816128165e67ee1174dbc7f993
Karl Bartel <karl@karl.berlin>
2021-08-11 13:28:27 +0200

    Inline `analyze` function
    It blocks some refactoring in its current form. We need to provide a
    clean public API once we have done more refactoring. For now, let's just
    keep the examples working.

--- causing/examples/__main__.py
+++ causing/examples/__main__.py
@@ -3,10 +3,11 @@ from pathlib import Path
 import json
 import warnings
 
-from causing import analyze
+from causing import utils, estimate
 from causing.examples import models
 from causing.utils import print_output, round_sig_recursive
-from causing.graph import create_graphs, sym_to_str
+from causing.graph import create_graphs, create_json_graphs, sym_to_str
+from causing.indiv import create_indiv
 
 # Our examples should run without any warnings, so let's treat them as errors.
 warnings.filterwarnings("error")
@@ -23,26 +24,32 @@ except AttributeError:
     print(f'Unkown model function "{model_name}".')
     exit(1)
 
+# Do all calculations
 model_raw_dat = model_function()
-analyze_dat = analyze(model_raw_dat)
-graphs = analyze_dat["graph_dat"]
+model_dat = utils.create_model(model_raw_dat)
+indiv_dat = create_indiv(model_dat)
+estimate_dat = estimate.estimate_models(model_dat)
+graphs = create_json_graphs(model_dat, estimate_dat, indiv_dat)
 # Round to 6 significant figures to make results stable even with minor floating point inaccuracies
 graphs = round_sig_recursive(graphs, sig=6)
 
+# Print text log
 output_dir = Path("output") / model_name
 output_dir.mkdir(parents=True, exist_ok=True)
 print_output(
-    analyze_dat["model_dat"],
-    analyze_dat["estimate_dat"],
-    analyze_dat["indiv_dat"],
+    model_dat,
+    estimate_dat,
+    indiv_dat,
     output_dir,
 )
+
+# Print json output
 with open(output_dir / "graphs.json", "w") as f:
     json.dump(graphs, f, sort_keys=True, indent=4)
 
-graphs["xnodes"] = [str(var) for var in analyze_dat["model_dat"]["xvars"]]
-graphs["ynodes"] = [str(var) for var in analyze_dat["model_dat"]["yvars"]]
+# Draw graphs
+graphs["xnodes"] = [str(var) for var in model_dat["xvars"]]
+graphs["ynodes"] = [str(var) for var in model_dat["yvars"]]
 graphs["is_all_graph"] = True
 graphs["final_var_is_rat_var"] = False
-
 create_graphs(graphs, output_dir, {})
