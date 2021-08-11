aa5703910136f1040739b39afb86ce231341ccf0
Karl Bartel <karl@karl.berlin>
2021-08-11 13:28:27 +0200

    Inline `analyze` function
    It blocks some refactoring in its current form. We need to provide a
    clean public API once we have done more refactoring. For now, let's just
    keep the examples working.

--- causing/__init__.py
+++ causing/__init__.py
@@ -1,25 +1,4 @@
 # -*- coding: utf-8 -*-
 """causing - causal interpretation using graphs."""
 
-from causing.utils import create_model, print_output
-from causing.estimate import estimate_models
-from causing.indiv import create_indiv
-from causing.graph import create_json_graphs
-
-
-def analyze(model_raw_dat, estimate=True):
-    model_dat = create_model(model_raw_dat)
-    if estimate:
-        estimate_dat = estimate_models(model_dat)
-    else:
-        estimate_dat = None
-    indiv_dat = create_indiv(model_dat)
-
-    graphs = create_json_graphs(model_dat, estimate_dat, indiv_dat)
-    ret_value = {
-        "model_dat": model_dat,
-        "indiv_dat": indiv_dat,
-        "graph_dat": graphs,
-        "estimate_dat": estimate_dat,
-    }
-    return ret_value
+# TODO: import public API here
