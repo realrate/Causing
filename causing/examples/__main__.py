0d5a1cb8f8f540cba354c4af5feb52bf596b62fb
Karl Bartel <karl@karl.berlin>
2021-08-11 10:18:40 +0200

    Fail on warning when running examples
    This prevents developers from introducing new warnings and gives us full
    tracebacks for the first warning on a run.

--- causing/examples/__main__.py
+++ causing/examples/__main__.py
@@ -1,10 +1,15 @@
 from sys import argv
 from pathlib import Path
 import json
+import warnings
+
 from causing import analyze
 from causing.examples import models
 from causing.utils import create_model, print_output, round_sig_recursive
 from causing.graph import create_graphs, sym_to_str
+
+# Our examples should run without any warnings, so let's treat them as errors.
+warnings.filterwarnings("error")
 
 if len(argv) != 2:
     print('Please call with model name as argument (e.g. "example" or "education").')
