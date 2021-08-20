04bc76aec6a0a21d552f06c0921bd6adf7113a39
Karl Bartel <karl@karl.berlin>
2021-08-20 12:09:04 +0200

    Expose important functions directly under `causing`
    Causing users should not need to know in which sub-package a specific
    function is implemented. We do not have that much functionality that we
    have to split it into namespaces for the user.

--- causing/__init__.py
+++ causing/__init__.py
@@ -2,3 +2,8 @@
 """causing - causal interpretation using graphs."""
 
 # TODO: import public API here
+from causing.estimate import estimate_models
+from causing.indiv import create_indiv
+from causing.graph import create_json_graphs
+from causing.model import Model
+from causing.simulate import simulate, SimulationParams
