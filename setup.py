9bebc71463ef84f67811d36cae29cdfcaa6ca8c4
salistha <salistha.shakya@realrate.ai>
2022-01-06 12:43:42 +0545

    update start your own model in readme with typo checks

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Causing: CAUSal INterpretation using Graphs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/realrate/RealRate-Causing",
+    url="https://github.com/realrate/Causing",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
