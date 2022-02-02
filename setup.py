3bc78d5b384b54287a39ae704e4a669c364d1bff
Karl Bartel <karl@karl.berlin>
2022-02-02 12:56:18 +0100

    Bump version number to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="causing",
-    version="0.1.7",
+    version="0.2.0",
     author="Dr. Holger Bartel",
     author_email="holger.bartel@realrate.ai",
     description="Causing: CAUSal INterpretation using Graphs",
